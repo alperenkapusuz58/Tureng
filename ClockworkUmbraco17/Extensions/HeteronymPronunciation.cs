3a410f7c0cd0feac074cb57fddfa763cd688f497
Alperen Kapusuz <alperen.kapusuz@clockwork.com.tr>
2026-07-09 21:39:32 +0300

    .

---
+++ ClockworkUmbraco17/Extensions/HeteronymPronunciation.cs
@@ -0,0 +1,19 @@
+﻿public static class HeteronymPronunciation
+{
+    private static readonly Dictionary<(string Word, string Pos), string> IpaOverrides =
+        new()
+        {
+            [("advocate", "noun")] = "ˈædvəkət",
+            [("advocate", "verb")] = "ˈædvəkeɪt",
+            [("record", "noun")] = "ˈrɛkɔːrd",
+            [("record", "verb")] = "rɪˈkɔːrd",
+            [("present", "noun")] = "ˈprɛzənt",
+            [("present", "verb")] = "prɪˈzɛnt",
+        };
+
+    public static string? ResolveIpa(string word, string partOfSpeech)
+    {
+        var key = ((word ?? "").Trim().ToLowerInvariant(), (partOfSpeech ?? "").Trim().ToLowerInvariant());
+        return IpaOverrides.TryGetValue(key, out var ipa) ? ipa : null;
+    }
+}
--- ClockworkUmbraco17/Extensions/PublishedContentExtensions.cs
+++ ClockworkUmbraco17/Extensions/PublishedContentExtensions.cs
@@ -75,6 +75,7 @@ namespace ClockworkUmbraco.Extensions
             return metin;
         }
 
+
     }
 }
 
